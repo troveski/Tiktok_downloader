00b884e1fcd60e7c64ba9525b199d35c07b39766
troveski <alvaro@LAPTOP-7T4OH9O1>
2025-02-09 09:19:39 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, send_file
+import yt_dlp
+import os
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        url = request.form['url']
+        if 'tiktok.com' in url:
+            try:
+                ydl_opts = {
+                    'outtmpl': 'downloads/%(title)s.%(ext)s',
+                    'format': 'best',
+                }
+                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                    info_dict = ydl.extract_info(url, download=True)
+                    video_title = ydl.prepare_filename(info_dict)
+                    return send_file(video_title, as_attachment=True)
+            except Exception as e:
+                return f"Error: {str(e)}"
+        else:
+            return "Invalid URL. Please enter a valid TikTok URL."
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(port=5001)
+    if not os.path.exists('downloads'):
+        os.makedirs('downloads')
+    app.run(debug=True)
